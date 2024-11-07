6c915c1c4bf71316674c15f2a3e83ab3a56df5d9
Rosana <annros2003@gmail.com>
2024-11-07 14:25:24 -0800

    handled add contact row button

--- force-app/main/default/lwc/contactCreation/contactCreation.js
+++ force-app/main/default/lwc/contactCreation/contactCreation.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
 
-export default class ContactCreation extends LightningElement {}
+export default class ContactCreation extends LightningElement {
+    contactRows = [];
+    handleAddContactRow() {
+        const newContact = {
+            id: this.contactRows.length + 1,
+            firstName: '',
+            lastName: '',
+            dateOfBirth: '',
+            email: '',
+            leadSource: ''
+        };
+
+        this.contactRows = [...this.contactRows, newContact];
+    }
+
+}
