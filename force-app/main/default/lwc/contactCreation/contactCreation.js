bdefadc72d138bd5b2b5ac9a40a53c47e4af86c6
Rosana <annros2003@gmail.com>
2024-11-09 09:51:29 -0800

    implemented basic 'create contacts'

--- force-app/main/default/lwc/contactCreation/contactCreation.js
+++ force-app/main/default/lwc/contactCreation/contactCreation.js
@@ -1,10 +1,14 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track } from 'lwc';
+import CONTACT_OBJECT from '@salesforce/schema/Contact';
+import { createRecord } from 'lightning/uiRecordApi';
+import { ShowToastEvent } from 'lightning/platformShowToastEvent';
 
 export default class ContactCreation extends LightningElement {
-    contactRows = [];
+    @track contacts = [];
+
     handleAddContactRow() {
         const newContact = {
-            id: this.contactRows.length + 1,
+            id: this.contacts.length + 1,
             firstName: '',
             lastName: '',
             dateOfBirth: '',
@@ -12,7 +16,78 @@ export default class ContactCreation extends LightningElement {
             leadSource: ''
         };
 
-        this.contactRows = [...this.contactRows, newContact];
+        this.contacts = [...this.contacts, newContact];
+    }
+
+    handleInputChange(event) {
+        const contactId = event.target.dataset.id;
+        const fieldName = event.target.dataset.field;
+        const inputValue = event.target.value;
+
+        this.contacts = this.contacts.map(contact => {
+            if (String(contact.id) === contactId) {
+                return {...contact, [fieldName]: inputValue};
+            }
+            return contact;
+        });
+
+        console.log('contacts:', JSON.stringify(this.contacts));
+    }
+
+    handleCreateContacts() {
+        const promises = this.contacts.map(contact => {
+            const fields = {
+                FirstName: contact.firstName,
+                LastName: contact.lastName,
+                Birthdate: contact.dateOfBirth,
+                Email: contact.email,
+                LeadSource: contact.leadSource
+            };
+    
+            const recordInput = { apiName: CONTACT_OBJECT.objectApiName, fields };
+    
+            return createRecord(recordInput);
+        });
+
+        Promise.all(promises)
+            .then(() => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Success',
+                        message: 'Contacts created successfully!',
+                        variant: 'success'
+                    })
+                );
+            })
+            .catch(error => {
+                this.dispatchEvent(
+                    new ShowToastEvent({
+                        title: 'Error creating Contacts',
+                        message: 'Cannot create contacts. Error: ' + error.body.message,
+                        variant: 'error'
+                    })
+                );
+            })
+        
+        // createContacts({contacts: this.contacts})
+        //     .then(() => {
+        //         this.dispatchEvent(
+        //             new ShowToastEvent({
+        //                 title: 'Success',
+        //                 message: 'Contacts created successfully!',
+        //                 variant: 'success'
+        //             })
+        //         );
+        //     })
+        //     .catch(error => {
+        //         this.dispatchEvent(
+        //             new ShowToastEvent({
+        //                 title: 'Error creating Contacts',
+        //                 message: 'Cannot create contacts. Error: ' + error.body.message,
+        //                 variant: 'error'
+        //             })
+        //         );
+        //     })
     }
 
 }
